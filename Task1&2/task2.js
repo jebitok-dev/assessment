d799a2ed4aaa009b9275dabf63e5dd25d63e8cb2
jebitok-dev <sharonkosgei4@gmail.com>
2020-10-19 06:26:44 +0300

    task 2 & nodemysql

--- Task1&2/task2.js
+++ Task1&2/task2.js
@@ -2,3 +2,30 @@
 // groups of four, and returns the transformed string.
 // e.g. when S = 'Lorem at' the output should be 'eroLta m'
 // when S = ' Tempor ip' the output should be 'meT roppi'
+
+// function transformedString(str) {
+//     str = str.split('') 
+//     let length = str.length,
+//         n = Math.floor(length / 2),
+//         tmp;
+        
+//         for(let i = 1; i <= n; i++) {
+//             tmp = str[length];
+//             str[length] = str[1];
+//             str[i] = tmp;
+//         }
+//         return str.join('')
+// }
+
+// console.log(transformedString('shawn men'))
+
+// var str = 'Lorem at';
+// var strLength = str.length;
+
+// console.log(str.substring(0 , (strLength / 2)));
+// console.log(str.substring(strLength/2));
+
+var str = "Hello world!";
+var res = str.substring(str.length);
+
+console.log(res)
